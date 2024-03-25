9c1d25dc2373785daf0be51a8f8061e56d89d12b
Vinncz <kevgun03@gmail.com>
2024-03-25 14:45:33 +0700

    Initial Commit

---
+++ Blink/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by Vin on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
